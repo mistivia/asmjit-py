1688331fc4d0ed970161b39577fee80066c6d5b6
Mistivia <i@mistivia.com>
2026-08-26 07:51:33 +0800

    add run.py

---
+++ tests/run.py
@@ -0,0 +1,16 @@
+from test_basic import test_function_returning_42
+from test_lea import test_lea
+from test_mov import test_mov
+from test_movsd import test_movsd
+from test_movsx import test_movsx
+from test_movzx import test_movzx
+from test_sib import test_sib
+
+
+test_function_returning_42()
+test_mov()
+test_movzx()
+test_movsx()
+test_sib()
+test_lea()
+test_movsd()
