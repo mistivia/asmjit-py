b76e73279ae2208ec1438721d0528f775fc7a72b
Mistivia <i@mistivia.com>
2026-08-29 20:42:44 +0800

    add aligned mov test

--- tests/test_simd.py
+++ tests/test_simd.py
@@ -12,6 +12,8 @@ def test_simd() -> None:
     result_storage = (ctypes.c_ubyte * 63)()
     source_address = (ctypes.addressof(source_storage) + 31) & ~31
     result_address = (ctypes.addressof(result_storage) + 31) & ~31
+    assert source_address % 32 == 0
+    assert result_address % 32 == 0
     source = (ctypes.c_float * 8).from_address(source_address)
     result = (ctypes.c_float * 8).from_address(result_address)
     source[:] = range(8)
@@ -19,7 +21,11 @@ def test_simd() -> None:
     e = Emitter()
     e.label('f')
     e.vmovaps(ymm0, m256_ptr(rdi))
-    e.vmovaps(m256_ptr(rsi), ymm0)
+    e.vmovaps(ymm1, ymm0)
+    e.vmovaps(m256_ptr(rsi), ymm1)
+    e.vmovaps(xmm2, m128_ptr(rdi))
+    e.vmovaps(xmm3, xmm2)
+    e.vmovaps(m128_ptr(rsi), xmm3)
     e.ret()
     e.finalize()
     _ = ccall(e.symbol('f'), source_address, result_address)
